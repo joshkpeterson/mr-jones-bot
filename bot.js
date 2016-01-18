87e0dd44b1122a2245a085b535d3b515d70af274
Josh <joshkpete@gmail.com>
2016-01-18 17:24:51 -0500

    Add some boilerplate code

--- bot.js
+++ bot.js
@@ -4,38 +4,75 @@ var Twit = require('twit');
 // We need to include our configuration file
 var T = new Twit(require('./config.js'));
 
-// This is the URL of a search for the latest tweets on the '#mediaarts' hashtag.
-var mediaArtsSearch = {q: "#mediaarts", count: 10, result_type: "recent"}; 
-
-// This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
-function retweetLatest() {
-	T.get('search/tweets', mediaArtsSearch, function (error, data) {
-	  // log out any errors and responses
-	  console.log(error, data);
-	  // If our search request to the server had no errors...
-	  if (!error) {
-	  	// ...then we grab the ID of the tweet we want to retweet...
-		var retweetId = data.statuses[0].id_str;
-		// ...and then we tell Twitter we want to retweet it!
-		T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
-			if (response) {
-				console.log('Success! Check your bot, it should have retweeted something.')
-			}
-			// If there was an error with our Twitter call, we print it out here.
-			if (error) {
-				console.log('There was an error with Twitter:', error);
-			}
-		})
-	  }
-	  // However, if our original search request had an error, we want to print it out here.
-	  else {
-	  	console.log('There was an error with your hashtag search:', error);
-	  }
-	});
+function checkThrottled() {
+	T.get('application/rate_limit_status', {resources: 'statuses'}, function (err, data, response) {
+		// console.log(JSON.stringify(data));
+		console.log(data.resources.statuses['/statuses/mentions_timeline'].remaining);
+		// console.log(data);
+	})
 }
 
-// Try to retweet something as soon as we run the program...
-retweetLatest();
-// ...and then every hour after that. Time here is in milliseconds, so
-// 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
-setInterval(retweetLatest, 1000 * 60 * 60);
+checkThrottled();
+
+//////////////////////////////////////////////
+
+//twitter data
+var latestMentions = [];  
+var idStrings = {};
+
+
+app.get('/*', function(req, res){  
+  res.send('Hello World');
+});
+
+var server = app.listen(port, function(){  
+  console.log('Basic server is listening on port ' + port);
+});
+
+var getMentions = function(){  
+  twit.get('/statuses/mentions_timeline.json', {count: 10}, function(data){
+    if(data.length){
+      for(var i = 0; i < data.length; i++){
+        var currentTweet = data[i];
+        if(!idStrings[currentTweet.id_str]){
+        	idStrings[currentTweet.id_str] = true;
+
+        	//
+        	// Do regex for "who" here, and if found, then add to latestmentions
+
+
+          var tweetObj = {};
+          tweetObj.user = currentTweet.user.screen_name;
+          tweetObj.text = currentTweet.text;
+          latestMentions.push(tweetObj);
+        }
+      }
+      //response to new mentions
+      replyToMentions();
+    } 
+    else{
+      console.log(data);
+    }
+  });
+};
+
+//This function takes all of the mentions stored in our latestMentions array and responds to them
+//with a simple message. We want to invoke it at the end of our getMentions function, so it is called
+//when we have all our new mentions. 
+var replyToMentions = function(){  
+  for(var i = 0; i < latestMentions.length; i++){
+    var currentMention = latestMentions[i];
+    //responseTweet is the string we will send to twitter to tweet for us
+    var responseTweet = 'Hello @';
+    responseTweet += currentMention.user;
+    responseTweet += '\nI hope you are having a wonderful day! \n-Your Favorite Node Server';
+
+    //twit will now post this responseTweet to twitter. This function takes a string and a callback
+    twit.updateStatus(responseTweet, function(){
+      console.log(responseTweet);
+    });
+  }
+};
+
+getMentions();
+
